00ee62e6e84789ded17c1ea43dc1b47fffc3e5a8
ghubstan <36207203+ghubstan@users.noreply.github.com>
2023-04-12 15:29:54 -0300

    Force panels to relayout after select

--- demos-0-9-1/DynamicLists1/src/commonMain/kotlin/io/dongxi/page/panel/CenterPanel.kt
+++ demos-0-9-1/DynamicLists1/src/commonMain/kotlin/io/dongxi/page/panel/CenterPanel.kt
@@ -5,15 +5,20 @@ import io.dongxi.page.panel.event.BaseProductSelectEventBus
 import io.nacular.doodle.animation.Animator
 import io.nacular.doodle.controls.PopupManager
 import io.nacular.doodle.controls.modal.ModalManager
+import io.nacular.doodle.drawing.Color
 import io.nacular.doodle.drawing.FontLoader
 import io.nacular.doodle.drawing.TextMetrics
 import io.nacular.doodle.focus.FocusManager
 import io.nacular.doodle.geometry.PathMetrics
 import io.nacular.doodle.geometry.Size
 import io.nacular.doodle.image.ImageLoader
+import io.nacular.doodle.layout.HorizontalFlowLayout
 import io.nacular.doodle.theme.ThemeManager
 import io.nacular.doodle.theme.adhoc.DynamicTheme
 import io.nacular.doodle.theme.native.NativeHyperLinkStyler
+import io.nacular.doodle.utils.Dimension
+import io.nacular.doodle.utils.HorizontalAlignment
+import io.nacular.doodle.utils.VerticalAlignment
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.filterNotNull
@@ -51,13 +56,30 @@ class CenterPanel(
     baseProductSelectEventBus
 ) {
 
+    private val tempLabel = io.nacular.doodle.controls.text.Label(
+        "NADA",
+        VerticalAlignment.Middle,
+        HorizontalAlignment.Center
+    ).apply {
+        height = 24.0
+        fitText = setOf(Dimension.Width)
+        foregroundColor = Color.Transparent
+    }
+
     init {
         size = Size(200, 200)
+
+        children += listOf(tempLabel)
+        layout = HorizontalFlowLayout()
 
         mainScope.launch {
             baseProductSelectEventBus.events.filterNotNull().collectLatest {
                 currentBaseProduct = it.baseProductDetail()
                 println("CenterPanel currentBaseProduct: $currentBaseProduct")
+
+                tempLabel.text =
+                    "${currentBaseProduct.productCategory.name ?: ""} ${currentBaseProduct.name ?: ""} ${currentBaseProduct.file ?: ""}"
+                relayout()
             }
         }
     }
