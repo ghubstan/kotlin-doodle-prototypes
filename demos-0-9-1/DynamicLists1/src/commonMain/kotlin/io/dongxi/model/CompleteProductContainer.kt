0834929e53c5ebee132614dac15acff5033234e3
ghubstan <36207203+ghubstan@users.noreply.github.com>
2023-04-20 12:31:42 -0300

    WIP reduce # of complete product container impls to one

--- demos-0-9-1/DynamicLists1/src/commonMain/kotlin/io/dongxi/model/CompleteProductContainer.kt
+++ demos-0-9-1/DynamicLists1/src/commonMain/kotlin/io/dongxi/model/CompleteProductContainer.kt
@@ -1,4 +1,84 @@
 package io.dongxi.model
 
-class CompleteProductContainer {
+import io.dongxi.application.DongxiConfig
+import io.dongxi.page.MenuEventBus
+import io.dongxi.page.PageType
+import io.dongxi.page.panel.event.AccessorySelectEventBus
+import io.dongxi.page.panel.event.BaseProductSelectEventBus
+import io.nacular.doodle.animation.Animator
+import io.nacular.doodle.controls.PopupManager
+import io.nacular.doodle.controls.modal.ModalManager
+import io.nacular.doodle.drawing.FontLoader
+import io.nacular.doodle.drawing.TextMetrics
+import io.nacular.doodle.focus.FocusManager
+import io.nacular.doodle.geometry.PathMetrics
+import io.nacular.doodle.geometry.Size
+import io.nacular.doodle.image.ImageLoader
+import io.nacular.doodle.layout.constraints.constrain
+import io.nacular.doodle.theme.ThemeManager
+import io.nacular.doodle.theme.adhoc.DynamicTheme
+import io.nacular.doodle.theme.native.NativeHyperLinkStyler
+import kotlinx.coroutines.CoroutineDispatcher
+
+class CompleteProductContainer(
+    pageType: PageType,
+    config: DongxiConfig,
+    uiDispatcher: CoroutineDispatcher,
+    animator: Animator,
+    pathMetrics: PathMetrics,
+    fonts: FontLoader,
+    theme: DynamicTheme,
+    themes: ThemeManager,
+    images: ImageLoader,
+    textMetrics: TextMetrics,
+    linkStyler: NativeHyperLinkStyler,
+    focusManager: FocusManager,
+    popups: PopupManager,
+    modals: ModalManager,
+    menuEventBus: MenuEventBus,
+    baseProductSelectEventBus: BaseProductSelectEventBus,
+    accessorySelectEventBus: AccessorySelectEventBus
+) : ICompleteProductContainer, AbstractCompleteProductContainer(
+    pageType,
+    config,
+    uiDispatcher,
+    animator,
+    pathMetrics,
+    fonts,
+    theme,
+    themes,
+    images,
+    textMetrics,
+    linkStyler,
+    focusManager,
+    popups,
+    modals,
+    menuEventBus,
+    baseProductSelectEventBus
+) {
+    init {
+        updateDebugLabelText()
+        clipCanvasToBounds = false
+        size = Size(200, 200)
+
+        children += listOf(debugLabel, productPhoto, accessoryPhoto)
+        layout = constrain(debugLabel, productPhoto, accessoryPhoto) { debugLabelBounds,
+                                                                       necklacePhotoBounds,
+                                                                       pendantPhotoBounds ->
+            debugLabelBounds.top eq 5
+            debugLabelBounds.left eq 5
+            debugLabelBounds.width.preserve
+            debugLabelBounds.height.preserve
+
+            necklacePhotoBounds.top eq debugLabelBounds.bottom + 10
+            necklacePhotoBounds.left eq 10
+            necklacePhotoBounds.width.preserve
+            necklacePhotoBounds.height.preserve
+
+            pendantPhotoBounds.left eq accessoryPhotoLeftBounds
+            pendantPhotoBounds.centerY eq accessoryPhotoCenterYBounds
+            pendantPhotoBounds.width.preserve
+            pendantPhotoBounds.height.preserve
+        }
+    }
 }
